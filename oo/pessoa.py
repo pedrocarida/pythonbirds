c811d995eaa2843ecf9b206390217c19b95e265f
pedro <pedrocarida@hotmail.com>
2020-03-25 18:15:31 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,19 @@
 class Pessoa:
+    def __init__(self, *filhos, nome=None, idade=28):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-
-
+    eduardo = Pessoa(nome='Eduardo')
+    pedro = Pessoa(eduardo, nome='Pedro')
+    print(Pessoa.cumprimentar(pedro))
+    print(id(pedro))
+    print(pedro.cumprimentar())
+    print(pedro.nome)
+    print(pedro.idade)
+    for filho in pedro.filhos:
+        print(filho.nome)
